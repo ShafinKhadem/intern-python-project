51848d60c829f30fcaa3f9cf808753cbd67364e4
Nafiur Rahman Khadem <nafiur.khadem@optimizely.com>
2022-08-08 15:08:21 +0600

    complete task

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
+import time
+from getpass import getpass
+
+from pyjokes import get_joke
+
+from util import TweeterRequest
+
 
 def main():
-    pass
+    print("Please login to your acocuont")
+    username = input("Username: ")
+    password = getpass()
+    print("Logging in ...")
+    try:
+        tweeter = TweeterRequest(username, password)
+        print("log in successful")
+        print("checking recent tweets ...")
+        recent_twets = tweeter.get_recent_tweets()
+        for tweet in recent_twets:
+            print(f"({tweet['id']}) {tweet['author']['username']} tweeted at {tweet['created_at']}")
+            print(tweet["text"])
+            print()
+        existing_jokes = set([tweet["text"] for tweet in recent_twets])
+        for i in range(10):
+            joke = ""
+            while True:
+                joke = get_joke()
+                if joke not in existing_jokes:
+                    existing_jokes.add(joke)
+                    break
+            print("posting tweet")
+            print(joke)
+            tweeter.post_tweet(joke)
+            print("posted tweet. sleeping 1 min now")
+            time.sleep(60)
+    except Exception:
+        print("try again")
+        raise
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- util.py
+++ util.py
@@ -1,6 +1,8 @@
 import json
+import time
+from functools import wraps
+from urllib.error import HTTPError, URLError
 from urllib.request import Request, urlopen
-from urllib.error import HTTPError
 
 """
 TODO: Write whatever class or function you may need
@@ -11,28 +13,95 @@ feel free to make any changes in the given class and its methods' arguments or i
 
 class NetworkRequest:
     @staticmethod
-    def get(url, headers = {}):
-        req = Request(url=url, method='GET')
-        for key, value in headers.items():
-            req.add_header(key, value)
-        
+    def _request(req):
         result = {}
-        with urlopen(req) as res:
-            body = res.read().decode('utf-8')
-            result['body'] = json.loads(body)
-            result['code'] = res.status
-        
+        try:
+            with urlopen(req) as res:
+                body = res.read().decode("utf-8")
+                result["body"] = json.loads(body)
+                result["code"] = res.status
+        except HTTPError as error:
+            result["code"] = error.status
+            print(error.status, error.reason)
+        except URLError as error:
+            print(error.reason)
+        except TimeoutError:
+            print("Request timed out")
         return result
-    
-    @staticmethod
-    def post():
-        pass
 
     @staticmethod
-    def put():
-        pass
+    def get(url, data={}, headers={}):
+        req = Request(url=url, data=json.dumps(data).encode("utf-8"), headers=headers, method="GET")
+        req.add_header("Content-type", "application/json")
+        return NetworkRequest._request(req)
 
     @staticmethod
-    def delete():
-        pass
+    def post(url, data={}, headers={}):
+        req = Request(url=url, data=json.dumps(data).encode("utf-8"), headers=headers, method="POST")
+        req.add_header("Content-type", "application/json")
+        return NetworkRequest._request(req)
 
+    @staticmethod
+    def put(url, data={}, headers={}):
+        req = Request(url=url, data=json.dumps(data).encode("utf-8"), headers=headers, method="PUT")
+        req.add_header("Content-type", "application/json")
+        return NetworkRequest._request(req)
+
+    @staticmethod
+    def delete(url, data={}, headers={}):
+        req = Request(url=url, data=json.dumps(data).encode("utf-8"), headers=headers, method="DELETE")
+        req.add_header("Content-type", "application/json")
+        return NetworkRequest._request(req)
+
+
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        elapsed_time = end_time - start_time
+        print(f"(time taken: {elapsed_time} seconds)")
+        return result
+
+    return timeit_wrapper
+
+
+class TweeterRequest:
+    API_URL = "https://intern-test-server.herokuapp.com/api"
+
+    def __init__(self, username, password) -> None:
+        tokens = NetworkRequest.post(f"{type(self).API_URL}/auth", {"username": username, "password": password})["body"]
+        self.access_token = tokens["access_token"]
+        self.refresh_token = tokens["refresh_token"]
+
+    def refresh_token_on_failure(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            result = func(self, *args, **kwargs)
+            if result["code"] == 401:
+                print("refreshing token")
+                new_tokens = NetworkRequest.post(
+                    f"{type(self).API_URL}/auth/token", {"refresh_token": self.refresh_token}
+                )["body"]
+                self.access_token, self.refresh_token = new_tokens["access_token"], new_tokens["refresh_token"]
+                result = func(self, *args, **kwargs)
+            return result["body"]
+
+        return wrapper
+
+    @timeit
+    @refresh_token_on_failure
+    def get_recent_tweets(self):
+        return NetworkRequest.get(
+            f"{type(self).API_URL}/tweets", headers={"Authorization": f"Bearer {self.access_token}"}
+        )
+
+    @timeit
+    @refresh_token_on_failure
+    def post_tweet(self, tweet):
+        return NetworkRequest.post(
+            f"{type(self).API_URL}/tweets",
+            data={"text": tweet},
+            headers={"Authorization": f"Bearer {self.access_token}"},
+        )
